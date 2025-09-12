98e0d127019f06a339f20bf0f3add113ec499826
Uros258 <broforceuros@gmail.com>
2025-09-12 14:53:39 +0200

    Dodato: putanja do slika soba u SobaSeeder

--- database/seeders/SobaSeeder.php
+++ database/seeders/SobaSeeder.php
@@ -10,9 +10,9 @@ class SobaSeeder extends Seeder
     public function run(): void
     {
         DB::table('sobas')->upsert([
-            ['id'=>1,'broj_sobe'=>101,'tip_sobe'=>'Single','cena'=>4500,'status_sobe'=>'slobodna','created_at'=>now(),'updated_at'=>now()],
-            ['id'=>2,'broj_sobe'=>102,'tip_sobe'=>'Double','cena'=>6500,'status_sobe'=>'slobodna','created_at'=>now(),'updated_at'=>now()],
-            ['id'=>3,'broj_sobe'=>201,'tip_sobe'=>'Suite','cena'=>12000,'status_sobe'=>'slobodna','created_at'=>now(),'updated_at'=>now()],
-        ], ['id'], ['broj_sobe','tip_sobe','cena','status_sobe','updated_at']);
+            ['id'=>1,'broj_sobe'=>101,'tip_sobe'=>'Single','cena'=>4500,'status_sobe'=>'slobodna','ukupno_soba' =>5, 'slika' => 'images/rooms/101.jpg', 'created_at'=>now(),'updated_at'=>now()],
+            ['id'=>2,'broj_sobe'=>102,'tip_sobe'=>'Double','cena'=>6500,'status_sobe'=>'slobodna','ukupno_soba' =>5, 'slika' => 'images/rooms/102.jpg', 'created_at'=>now(),'updated_at'=>now()],
+            ['id'=>3,'broj_sobe'=>201,'tip_sobe'=>'Suite','cena'=>12000,'status_sobe'=>'slobodna','ukupno_soba' =>4, 'slika' => 'images/rooms/201.jpg','created_at'=>now(),'updated_at'=>now()],
+        ], ['id'], ['broj_sobe','tip_sobe','cena','status_sobe','slika', 'ukupno_soba','updated_at']);
     }
 }
